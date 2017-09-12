bced065780da52d7f291e8525e10451f471670d8
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-09-12 15:33:16 -0400

    Names Finished

--- Vincent Zheng/src/intro/CodingConventions.java
+++ Vincent Zheng/src/intro/CodingConventions.java
@@ -13,7 +13,7 @@ public class CodingConventions {
 		/* this is a constructor, it is like a method except the "return type" is an instance of the class
 		 * for this reason, a constructor must ALWAYS be named after the class
 		 */
-		public CodingConventions(String name, int descriptionIndex) {
+		public CodingConventions(int nameIndex , int descriptionIndex) {
 			//instantiate variable now:
 			//there are two variables called "name" the local variable and the field
 			//distinguish between the two using the reserve word "this" (field)
@@ -21,6 +21,7 @@ public class CodingConventions {
 			
 			//this is a static call to the constant
 			description = IntroMain.DESCRIPTIONS[descriptionIndex];
+			name = IntroMain.NAMES[nameIndex];
 			
 		}
 		// normal method "void" is the return type
--- Vincent Zheng/src/intro/IntroMain.java
+++ Vincent Zheng/src/intro/IntroMain.java
@@ -1,10 +1,11 @@
 package intro;//package declaration: matches folder where this file is located
-//class header
+//class header-
+
 public class IntroMain {
 	
-	//this is aa constant, they can be public
+	//this is a constant, they can be public
 	public static final String[] DESCRIPTIONS = {" is a student at BTHS"," is a teacher at BTHS"}; 
-	
+	public static final String[] NAMES = {"Derek","VincentTheGod","Jonie","David","TheOtherDavid","Kevin","Steven"}; 
 	// this is the first method that is executed. It is static because it is independent of instances
 	public static void main(String[] args) {
 		//this is the declaration of an instance of CodingConventions
@@ -14,7 +15,7 @@ public class IntroMain {
 		
 		int numberOfPeople = 10;
 		for(int i = 0; i< numberOfPeople;i++) {
-			CodingConventions conventionsInstance = new CodingConventions("Vincent Zheng",i%2);
+			CodingConventions conventionsInstance = new CodingConventions(i%7,i%2);
 			//a note on local variables: they are NOT FIELDS (HAS-A relationships)
 			// they are variables that are only used in the scope of a method
 			// after the method, they are destroyed
