019f2723560efc04abad3aa4a59c3caee78a0c6d
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-09-12 15:18:08 -0400

    Coding Convention lesson 9/12/17

---
+++ Vincent Zheng/src/intro/CodingConventions.java
@@ -0,0 +1,32 @@
+package intro;
+
+public class CodingConventions {
+
+		//FIELDS ARE AT THE TOP!!
+		private String name;
+		/*
+		 * IOW don't do this:
+		 * private String name = "Vincent Zheng"
+		 */
+		private String description;
+		
+		/* this is a constructor, it is like a method except the "return type" is an instance of the class
+		 * for this reason, a constructor must ALWAYS be named after the class
+		 */
+		public CodingConventions(String name, int descriptionIndex) {
+			//instantiate variable now:
+			//there are two variables called "name" the local variable and the field
+			//distinguish between the two using the reserve word "this" (field)
+			this.name = name;
+			
+			//this is a static call to the constant
+			description = IntroMain.DESCRIPTIONS[descriptionIndex];
+			
+		}
+		// normal method "void" is the return type
+		public void doStuff() {
+			//static method call:
+			String output = name + description;// use spaces in between operation 1 + 1 = 2 not 1+1=2
+			System.out.println(output);
+		}
+}
