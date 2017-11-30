49e715965bebbe956d0c9e276d6d23cfb5c28887
BT_1N3_19 <BT_1N3_19@BT_1N3_19.bktech.local>
2017-11-30 15:33:15 -0500

    gg

---
+++ Vincent Zheng/src/algorithms/Action.java
@@ -0,0 +1,5 @@
+package algorithms;
+
+public interface Action {
+	public void act();
+}
---
+++ Vincent Zheng/src/algorithms/LionPuzzle.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+public class LionPuzzle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numberOfLions = 3;
+		if(willEat(numberOfLions)) {
+			System.out.println("Eat the sheep!");
+		}else {
+			System.out.println("Do not eat the sheep!");
+		}
+	}
+	
+	private static boolean willEat(int numberOfLions) {
+		if(numberOfLions == 1) {
+			return true;
+		}else {
+			return !willEat(numberOfLions-1);
+		}
+	}
+}
